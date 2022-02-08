209326565d4dc5e3b7f24e82a351f920016b6293
HaNaK0 <hampus@hampushuledal.com>
2022-02-08 19:50:34 +0100

    Fix Get a square on the screen #1

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,49 @@
 use bevy::prelude::*;
 
-fn hello_world() {
-    println!("Hello world");
-}
-
 #[derive(Component)]
 struct Person;
 
 #[derive(Component)]
 struct Name(String);
 
-struct  GreetTimer(Timer);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
-    commands.spawn().insert(Person).insert(Name("Mina Smolgård".to_string()));
-    commands.spawn().insert(Person).insert(Name("Killian".to_string()));
+/// Walks around and mutters for himself
+#[derive(Component)]
+struct Grubbler {
+    pub angle: f32,
+    pub angle_speed: f32,
 }
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-        println!("Hello {}!", name.0);
-        }
-    }
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            transform: Transform{
+                scale: Vec3::new(10.0, 10.0, 10.0),
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                color: Color::GREEN,
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(Grubbler{angle: 0.0, angle_speed: 1.0});
 }
 
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
+fn grubbler_system(time: Res<Time>, mut query: Query<(&mut Grubbler, &mut Transform)>) {
+    for (mut grubbler, mut transform) in query.iter_mut() {
+        grubbler.angle += grubbler.angle_speed * time.delta_seconds();
+        transform.translation.x = grubbler.angle.cos() * 30.0;
+        transform.translation.y = grubbler.angle.sin() * 30.0;
     }
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_startup_system(setup)
+        .add_system(grubbler_system)
         .run();
 }
